203d276b3401daacd8bd96812f0c1223633bcf3a
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-01-07 17:04:09 +0530

    Add files via upload
    datatypes, variables, io functions

---
+++ DAY_1.py
@@ -0,0 +1,5 @@
+
+print("Day 1 python at codegnan")
+print("karthikeya")
+print("i am performing some output operations")
+ 
---
+++ DAY_2.py
@@ -0,0 +1,20 @@
+# datatypes, varible and IO functions
+name="KARTHIKEYA" #string type
+age=50  #integer
+weight=100.25 #floar type
+boolean=True   #boolean type
+list_1=[7,8.5,8,"pass",(10,20)] #list of items
+tuple_1=(8,8.2,6,"pass","good") #tuple of items
+cards = {'A',2,3,4,5,6,7,8,9,10,'J','Q','K'} #pack of cards in set datatype
+student_marks={'maths':90,'english':85,'hindi':88} #dictionary datatype
+
+print("My name is:",name)
+print("My age is:", age)
+print("My name is:",name,"My age is:", age)
+print("My weight is:", weight)
+print("list of items is:", list_1)
+print("Tuple of items is:", tuple_1)
+print("My age is:", age)
+print("pack of cards in set datatype is:", cards)
+print("student_marks is:", student_marks)
+
