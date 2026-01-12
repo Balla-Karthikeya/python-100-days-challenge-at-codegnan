5751309a298f9300275c7ba8a1e4cc645d7b6b9a
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-01-12 17:04:53 +0530

    Add files via upload

---
+++ DAY_6_Conditional_Statements_And_Loops.py
@@ -0,0 +1,11 @@
+n=int(input())
+if n%2==0:
+    if n>-1:
+        print("Number is +ve even")
+    else:
+        print("Number is -ve even")
+else:
+    if n>=-1:
+        print("Number is +ve ood")
+    else:
+        print("Number is -ve odd")
