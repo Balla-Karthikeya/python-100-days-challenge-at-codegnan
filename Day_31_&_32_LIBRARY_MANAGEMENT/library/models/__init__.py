65d88f895d3295d6d7ad99bddddbdac328fcc8da
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-03-03 23:15:37 +0530

    Delete Day_31_&_32_LIBRARY_MANAGEMENT/library directory

--- Day_31_&_32_LIBRARY_MANAGEMENT/library/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .book import Book
-from .user import User
-from .admin import Admin
-__all__ = ["User", "Book", "Admin"]
--- Day_31_&_32_LIBRARY_MANAGEMENT/library/models/admin.py
+++
@@ -1,9 +0,0 @@
-from .user import User
-
-class Admin(User):
-    def __init__(self, userid, email, password, name):
-        super().__init__(userid, email, password, name)
-        self.role = "Admin"
-
-    def manage_book(self):
-        return True
