e2b6430018665a9ea8859e4e8f97a91237945003
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-03-05 10:09:47 +0530

    Fix merge conflict in DAY_1.py
    Removed merge conflict markers from DAY_1.py.

--- DAY_1.py
+++ DAY_1.py
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
+
 
 print("Day 1 python at codegnan")
 print("karthikeya")
 print("i am performing some output operations")
  
-=======
+
 
 print("Day 1 python at codegnan")
 print("karthikeya")
 print("i am performing some output operations")
  
->>>>>>> ba4c159c42104fd55ee5e25855b304bff716441d
