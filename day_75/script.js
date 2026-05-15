89b7c6759cfa785c5726099399752444268232cd
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-05-15 11:10:56 +0530

    Add files via upload

---
+++ day_75/script.js
@@ -0,0 +1,41 @@
+// dom = document object model
+
+// tage name = h1 , h2 , h3 , h4 , h5 , h6, p 
+
+// document.getElementsByTagName("h1")
+
+// let h1 = document.getElementsByTagName('h1')
+
+
+// document.getElementsByClassName("h1")
+
+// document.getElementById("id")
+
+// querySelector
+
+// querySelectorAll
+
+
+// let h1 = document.getElementById('h1')
+
+
+let h1 =document.getElementsByClassName('h1')
+
+
+// h1[0].style.backgroundImage=;
+
+
+
+
+
+// h1[0].innerHTML="HII THIS IS H1 TAG"
+
+console.log(h1);
+
+
+function click() {
+    
+h1[0].innerHTML="hii this is h1 tag"
+h1[0].style.color="red";
+}
+
