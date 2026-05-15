abab448464d8758ebd803cc816d279f0a186b04c
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-05-15 11:13:18 +0530

    Add files via upload

---
+++ day_76/script.js
@@ -0,0 +1,176 @@
+// // // Dom
+
+// // // dom = document object model
+// // // getElementById = is used to get the element by id
+// // // getElementsByClassName = is used to get the element by class name
+// // // getElementsByTagName = is used to get the element by tag name
+// // // querySelector = is used to get the element by selector
+// // // querySelectorAll = is used to get all the elements by selector
+
+// // // console.log(h1)
+
+// // // function
+// // // regular function
+// // // function name(params) {
+
+// // // }
+// // // globle
+// // // let h1 = document.getElementsByTagName('h1')
+
+let h1 = document.getElementsByTagName("h1");
+
+let data ="🛻👦😊";
+
+// function change() {
+//     h1[0].innerHTML = data;
+//     h1[0].style.color = "red";
+// }
+
+
+
+
+
+
+
+// // // parameter function
+// // // function change( a = data , b = "giiiiiiiiiiiiii") {
+
+// // //     h1[0].innerHTML= a+b
+// // //     h1[0].style.color="red"
+// // // // h1[0].innerHTML= data
+
+// // // }
+
+// // // //
+// // // function add(a = 10 , b = 20) {
+// // //     console.log(a+b);
+// // // }
+
+// // // add()
+
+// // // function add(a , b ) {
+// // //     console.log(a+b);
+// // // }
+// // // // function with argument
+// // // add(10,20)
+
+// // // return function
+// // function add(a , b ) {
+// //     return a+b
+// // }
+// // // console.log(add(10,20));
+// // console.log(add(50,30));
+
+// // // add 80 - sub 1000
+
+// // function sub(Fun, b) {
+// //   return b- Fun;
+// // }
+
+// // console.log(sub(add(50,30), 1000));
+
+
+// // daily task
+// function wokeUp() {
+//   console.log("wokeUp");
+// }
+
+// function freshup() {
+//   console.log("freshup");
+    
+// }
+
+// function breakfast() {
+//   console.log("breakfast");
+// }
+
+// function study() {
+//   console.log("study");
+// }
+
+
+// function sleep() {
+//   console.log("sleep");
+// }
+
+
+
+// function Puneeths(task1 , task2 , task3 , task4) {
+//     task1();
+//     task2();
+//     task3();
+//     task4();
+    
+// }
+
+// Puneeths(wokeUp , freshup , breakfast , study)
+
+
+// // function sumanth(task1, tsak2) {
+// //     console.log("task of sumanth")
+// //     task1();
+// //     tsak2();
+// // }
+
+
+
+// let sumanth = function (task1, tsak2){
+//     console.log("task of sumanth")
+//     task1();
+//     tsak2();
+// }
+
+// sumanth( sleep , study)
+
+
+
+
+
+// // regular function
+// // syntax
+// // function name(params) {
+// //     console.log("hello")
+// // } 
+// // name() // function call 
+
+// // parameter function
+// // function name( a , b) {
+// //     console.log("hello")
+// // }
+
+// // name(a,b) // function call
+
+
+// // function with argument
+// // function name( a , b) {
+// //     console.log("hello")
+// // }
+
+// // name(a,b) // function call
+
+// // return function
+// // function name( a , b) {
+// //     return "hello"
+// // }
+
+// // name(a,b) // function call
+
+// // callback function
+// // function name( a , b) {
+// //     console.log("hello")
+// // }
+
+// // name(a(),b) // function call
+
+// arrow function
+// syntax
+// let name = () => {
+//     console.log("hello")
+// }
+// name() // function call
+
+let start =()=>{
+    console.log("hiii");
+}
+
+start()
