b08867f55bb9a921d2d1969a72c88e9a7a7e6e99
Balla-Karthikeya <karthikeyaballa123@gmail.com>
2026-05-25 08:53:00 +0530

    Add files via upload

---
+++ day_78/note.js
@@ -0,0 +1,44 @@
+// datatypes
+// 1. string
+// 2. number
+// 3. boolean
+// 4. undefined
+// 5. null
+// 6. array
+// 7. object     {key : value}
+
+// operators
+// 1. arthmetic operator
+// 2. assignment operator
+// 3. comparison operator
+// 4. logical operator
+
+// conditional statements
+// 1. if
+// 2. if else
+// 3. if else if 
+
+// loops
+// 1. for
+// 2. while (condition) {
+
+// }
+
+// 3. do while (condition) { }
+
+// function => function is a block of code that is used to perform a task
+// types of function
+// 1. named function
+// 2. function expression
+// 3. function parameter
+// 4. function return
+// 5. function arguments
+// 6 callback function
+// 7. arrow function imp function introduced in es6
+
+
+// by default functions are sync
+// async function => function is a block of code that is used to perform a task
+
+
+// async , await , promise , fetch , map , filter
